365e8b3724ca894976134c8db535c39785d36fc0
Wridzer <wridzer12@gmail.com>
2024-09-04 13:17:29 +0200

    updated build sequence
    i missed a step and now it adds the shortcut

--- build.bat
+++ build.bat
@@ -1,3 +1,19 @@
+@echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 cd godot-4.3
+scons platform=windows vsproj=yes dev_build=yes
 msbuild godot.sln
+
+:: Go back to the directory where the batch file is located
+cd ..
+
+:: Check if godot.exe.lnk already exists
+if not exist godot.exe.lnk (
+    :: Create a shortcut to the Godot editor
+    echo Creating a shortcut for the Godot editor...
+    powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%cd%\godot.exe.lnk');$s.TargetPath='%cd%\godot-4.3\bin\godot.windows.editor.dev.x86_64.exe';$s.Save()"
+) else (
+    echo godot.exe shortcut already exists.
+)
+
+pause
