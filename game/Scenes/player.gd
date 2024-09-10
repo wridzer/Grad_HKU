45f0d0c920d3e560d08f340138b0e4362a436872
mijkolsmith <mijkolsmith@live.com>
2024-09-10 11:34:45 +0200

    added custom actions for player movement

--- game/Scenes/player.gd
+++ game/Scenes/player.gd
@@ -5,8 +5,8 @@ const SPEED = 100.0
 
 
 func _physics_process(delta: float) -> void:
-	var directiony := Input.get_axis("ui_up", "ui_down")
-	var directionx := Input.get_axis("ui_left", "ui_right")
+	var directiony := Input.get_axis("move_up", "move_down")
+	var directionx := Input.get_axis("move_left", "move_right")
 	
 	velocity.x = directionx * SPEED if directionx else move_toward(velocity.x, 0, SPEED)
 	velocity.y = directiony * SPEED if directiony else move_toward(velocity.y, 0, SPEED)
