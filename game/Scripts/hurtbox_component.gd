d463de0545d55f4f83744db90bf5f3c53382e938
mijkolsmith <mijkolsmith@live.com>
2024-09-12 14:31:20 +0200

    added assertion to hurtbox for health

--- game/Scripts/hurtbox_component.gd
+++ game/Scripts/hurtbox_component.gd
@@ -4,6 +4,10 @@ extends Area2D
 @export var health : HealthComponent
 
 
+func _ready() -> void:
+	assert(is_instance_valid(health), "Please assign a valid HealthComponent to HurtboxComponent")
+
+
 func _on_area_entered(hitbox: HitboxComponent) -> void:
 	if hitbox == null:
 		return
