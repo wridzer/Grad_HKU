6d3b8a518e486a8c67152669b516d481a69b0e65
mijkolsmith <mijkolsmith@live.com>
2024-10-14 12:00:42 +0200

    more performant distance checking

--- game/Scripts/enemy.gd
+++ game/Scripts/enemy.gd
@@ -6,10 +6,16 @@ extends CharacterBody2D
 @onready var state_machine: StateMachine = $StateMachine
 @onready var danger_sensor_component: DangerSensorComponent = $DangerSensorComponent
 
+# MAX_INT32
+var squared_distance: int = (1 << 31) - 1
 
 func _ready() -> void:
 	health_component.die.connect(die)
 	health_component.hit.connect(hit)
+
+
+func _process(delta: float) -> void:
+	squared_distance = global_position.distance_squared_to(Player.instance.global_position);
 
 
 func die() -> void:
