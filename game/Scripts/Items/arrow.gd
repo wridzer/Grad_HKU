0769ec24700e5bc81dd2d66a3c0f54c1c38d552a
mijkolsmith <mijkolsmith@live.com>
2024-11-27 12:24:50 +0100

    arrow could hit the player when shooting, added a start timer to enable hitbox, changed enemy collision

--- game/Scripts/Items/arrow.gd
+++ game/Scripts/Items/arrow.gd
@@ -2,9 +2,11 @@ class_name Arrow
 extends Node2D
 
 
-@onready var timer: Timer = $Timer
+@onready var start_timer: Timer = $StartTimer
+@onready var stop_timer: Timer = $StopTimer
 @onready var hitbox_component_arrow: HitboxComponent = $HitboxComponentArrow
 
+const STARTING_DISTANCE := 2
 const STOPPING_DISTANCE := 2
 const SPEED := 150.0
 var mouse_position: Vector2
@@ -14,7 +16,9 @@ var hit: bool
 
 func _ready() -> void:
 	look_at(mouse_position)
-	timer.wait_time = STOPPING_DISTANCE / SPEED
+	start_timer.wait_time = STARTING_DISTANCE / SPEED
+	stop_timer.wait_time = STOPPING_DISTANCE / SPEED
+	start_timer.start()
 
 
 func _physics_process(delta: float) -> void:
@@ -23,11 +27,15 @@ func _physics_process(delta: float) -> void:
 		global_position += direction * SPEED * delta
 
 
+func _on_start_timer_timeout() -> void:
+	hitbox_component_arrow.set_process_mode(Node.PROCESS_MODE_INHERIT)
+
+
 func _on_hitbox_component_arrow_body_entered(_body: Node2D) -> void:
-	timer.start()
+	stop_timer.start()
 
 
-func _on_timer_timeout() -> void:
+func _on_stop_timer_timeout() -> void:
 	hit = true
 
 
