590bc6da165aa2321f102e6420908243a7d74c12
mijkolsmith <mijkolsmith@live.com>
2024-12-10 15:00:30 +0100

    bugfix stay close to player goal

--- game/Scripts/GOAP/goals/stay_close_to_player_goal.gd
+++ game/Scripts/GOAP/goals/stay_close_to_player_goal.gd
@@ -10,11 +10,12 @@ func _is_goal_met() -> bool:
 	var data = Blackboard.get_data("npc_location")
 	var other_data = Blackboard.get_data("squared_follow_distance")
 	if is_instance_valid(data) && is_instance_valid(other_data):
-		var npc_pos : Vector2 = data
-		var player_pos : Vector2 = Player.instance.global_position
-		var squared_distance = npc_pos.distance_squared_to(player_pos)
-		var squared_follow_distance
+		var npc_pos: Vector2 = data
+		var player_pos: Vector2 = Player.instance.global_position
+		var squared_distance: float = npc_pos.distance_squared_to(player_pos)
+		var squared_follow_distance: float = other_data
 		return squared_distance < squared_follow_distance
+	
 	return false
 
 
