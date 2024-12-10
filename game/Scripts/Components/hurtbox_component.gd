effa97904f219f5dc126932fa67796236eb23ba2
mijkolsmith <mijkolsmith@live.com>
2024-12-10 15:51:52 +0100

    bugfix hurtbox errors

--- game/Scripts/Components/hurtbox_component.gd
+++ game/Scripts/Components/hurtbox_component.gd
@@ -9,8 +9,8 @@ func _ready() -> void:
 	assert(is_instance_valid(_health_component), "Please assign a valid _health_component to HurtboxComponent")
 
 
-func _on_area_entered(hitbox: HitboxComponent) -> void:
-	if !is_instance_valid(hitbox):
+func _on_area_entered(hitbox: Area2D) -> void:
+	if !is_instance_valid(hitbox as HitboxComponent):
 		return
 	
 	_health_component.take_damage(hitbox.damage)
