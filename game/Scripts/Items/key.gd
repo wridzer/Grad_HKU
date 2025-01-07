29e15c7dc53d699d6fbf61e99ae9c4e1cbe184c9
mijkolsmith <mijkolsmith@live.com>
2025-01-07 12:52:53 +0100

    key bug

--- game/Scripts/Items/key.gd
+++ game/Scripts/Items/key.gd
@@ -4,13 +4,20 @@ extends Node2D
 
 @onready var _actionable: Area2D = $Actionable
 
+@export var pickup_dialogue: DialogueResource
+
 
 func _ready() -> void:
+	print("test")
 	_actionable.action.connect(_pickup)
+	Blackboard.increment_data("keys", 1)
 
 
 func _pickup() -> void:
-	# Dialogue how much keys are left
-	#dialogue_manager.start_dialogue(null)
-	
+	# Tell the player how much keys are left
+	dialogue_manager.start_dialogue(pickup_dialogue)
+	Blackboard.increment_data("keys", -1)
+	var keys = Blackboard.get("keys")
+	print(keys)
+	print("picked up")
 	queue_free()
