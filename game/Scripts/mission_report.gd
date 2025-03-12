29d2e24da8c8f0e2b089d505a5d018334a28f850
mijkolsmith <mijkolsmith@live.com>
2025-03-12 17:46:48 +0100

    update mission report

--- game/Scripts/mission_report.gd
+++ game/Scripts/mission_report.gd
@@ -25,13 +25,30 @@ func do_mission_report() -> void:
 	data = Blackboard.get_data("npc_choices")
 	var npc_name: String = (data as Array)[-1] if data else "None"
 	
+	data = Blackboard.get_data("npc")
+	var npc_level: String = str((data as Npc).level) if is_instance_valid(data) else "17"
+	
+	data = Blackboard.get_data("sword_level")
+	var sword_level: String = str(data) if data else "17"
+	
+	data = Blackboard.get_data("shield_level")
+	var shield_level: String = str(data) if data else "17"
+	
+	data = Blackboard.get_data("bow_level")
+	var bow_level: String = str(data) if data else "17"
+	
+	
 	stats.text = \
-"Sword used " + sword_used_amount + " amount
-Shield used " + shield_used_amount + " amount
-Bow used " + bow_used_amount + " amount
-Fiends slain " + enemies_killed + "
-Fiends left alive " + enemies_alive + "
-Companion taken: " + npc_name
+"Sword used: " + sword_used_amount + " times
+Shield used: " + shield_used_amount + " times
+Bow used: " + bow_used_amount + " times
+Enemies slain: " + enemies_killed + "
+Enemies left alive: " + enemies_alive + "
+Companion taken: " + npc_name + "
+Companion level: " + npc_level + "
+Sword level: " + sword_level + "
+Shield level: " + shield_level + "
+Bow level: " + bow_level
 	
 	visible = true
 
